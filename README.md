f9035670bf3c7a62af086170b13a953da7b908da
Hauke Hain <hihain@users.noreply.github.com>
2014-08-25 00:20:44 +0200

    - Corrected wrong "Total Spent" calculation (error in reasoning): Renamed it in Accounting in "Total" and added a new "Total Spent" with a correct calculation: Previously it missed the point that you may have paid for Steam wallet funds and that you may haven't spent them yet.

--- README.md
+++ README.md
@@ -2,17 +2,19 @@ Steam Accounting Bookmarklet
 ============================
 
 Steam Accounting Bookmarklet is a JavaScript that gives you an overview about your spending within Steam.
-It basically sums up all your Steam purchases and compares them with the Steam wallet funds you once had by selling items on the market or buying them in the Steam store. Redeemed CD keys are only counted, not priced.
+It basically sums up all your Steam purchases and compares them with the Steam wallet funds you once had by selling items on the market or buying them in the Steam store. Redeemed CD keys are only counted, not priced. Besides the Steam wallet point of view there is also one number ("Total Invested") calculated that tells you how much real world money you invested in your Steam account.
 
 What does it do?
 ------
-This JavaScript adds 3 accounts: Ingame, Market and Store. The accounts are also totaled. You can find them on the right of your account page in the "Your Steam Account" box.
-- *Store*: Here you see how much money you spent on products in the store (Debit) and on adding funds to your Steam wallet (Credit). The number of transactions also includes complimentary products. If your "Store Transactions" amount is negative, you bought more Steam wallet funds in the store than products. You might have spent your funds on the market, at an in-game store or you still have funds in your Steam wallet. See also "Total" for more information.
+This JavaScript adds 4 accounts: Ingame, Market, Store and Total. There is also an number called "Total Invested" added. You can find them on the right of your account page in the "Your Steam Account" box.
+- *Store*: Here you see how much money you spent on products in the store (Debit) and on adding funds to your Steam wallet (Credit). The number of transactions also includes complimentary products. If your "Store Transactions" amount is negative, you bought more Steam wallet funds in the store than products. You might have spent your funds on the market, at an in-game store or you still have funds in your Steam wallet. See also "Total" for more information. By the way: You can see here how many CD keys you redeemed.
 - *Ingame*: You can see how much money you spent in in-game stores.
 - *Market*: Balances the funds you gained by selling items on the market and how much you spent. If your earnings are higher than your expenses "Market Transactions" is negative. Otherwise you spent funds you bought in the Steam store."
-- *Total*: "Total Spent" sums up all your expenses and earnings. If the number is negative, it shows you the amount of money you have left in your Steam wallet. Furthermore this is the place where you can see how many CD keys you redeemed.
+- *Total*: "Total" sums up all your expenses and earnings in terms of your Steam wallet. So "Debit" shows in this account your total spent. But it may include funds that you have not paid with you hard-earned money but gained from the Steam market. Look at the next number "Total Invested" for the real world money you invested in your Steam account. If the "Total" number is negative, it shows you the amount of money you have left in your Steam wallet. If the amount is positive, you bought products in the Steam store and didn't paid with your Steam wallet (but by creadit card or any other payment method supported by Steam). So only if you always paid with your Steam wallet, you can get a total of zero if you have spent all the fundy you bought via the Steam store.
+- *Total Invested*: This number shows you how much money you invested in your Steam account respectively how much you paid Valve over the years.
 
-To see the accounts and not only the totals you need to click on the "[+]" link. Below the accounts you can see the number of transactions you made.
+Important: To see the accounts and not only the totals you need to click on the "[+]" link. Below the accounts you can see the number of transactions you made.
+
 Debit means: funds left your Steam wallet; Credit: funds got added to your Steam wallet.
 
 Usage
@@ -25,7 +27,9 @@ javascript:(function(){var e=document.createElement("script");e.type="text/javas
 
 Limitations/Known Issues
 ------------------------
-Key redemptions (from retail purchasese, etc.) are only counted, not priced because Steam does not know how much or if you paid for them.
+Key redemptions (from retail purchases, etc.) are only counted, not priced. Steam does not know how much or if you paid for them.
+
+If your account displays more than one currency, the totals show only the first currency found. You can see all in your account. This should not be a problem anymore as Steam displays today only your native store currency.
 
 I just do not know what happens if you use a voucher for adding Steam funds. Let me know!
 
--- steamaccounting.js
+++ steamaccounting.js
@@ -1,13 +1,14 @@
 /*jQuery(document).ready(function(){*/
   if (/^https\:\/\/store\.steampowered\.com\/accoun.+/.test(location.href)) {
-    var totals={/*'transactions':[], */'currencyList':[], 'currencyTotals':[], 'transactionCount':0, 'cdkeyCount':0, 'creditsCount':0};
+    var currencies = [];
+    var totals={'debits':[], 'credits':[], 'wallet':[], 'debitsCount':0, 'creditsCount':0, 'cdkeyCount':0};
     
     jQuery.each(['store','ingame','market'], function( index, value ){
       var debits={'transactions':[], 'currencySum':[]};
       var credits={'transactions':[], 'currencySum':[]};
 
       /* initialize currencySum */
-      jQuery(totals.currencyList).each(function(){
+      jQuery(currencies).each(function(){
         debits.currencySum[this]=0;
         credits.currencySum[this]=0;
       });
@@ -15,7 +16,7 @@
       jQuery('#'+value+'_transactions .transactionRow').each(function(){
           var transaction = {
               'currency':jQuery(this).find('.transactionRowPrice').text().replace(/[\w\s-.,]/g, ''),
-              'price':Number(jQuery(this).find('.transactionRowPrice').text().replace(/[^\d.,-]/g, '').replace('\,','\.').replace(/-/g,'0')),
+              'price':toNumber(jQuery(this).find('.transactionRowPrice').text()),
 /*              'date':jQuery(this).find('.transactionRowDate').text(),
               'event':jQuery(this).find('.transactionRowEvent').text(),
               'description':jQuery(this).find('.transactionRowEvent .transactionRowTitle').text(),
@@ -29,9 +30,11 @@
 
           /* add new currencies */
           var currency_string = transaction.currency.toString();
-            if ((jQuery.inArray(transaction.currency, totals.currencyList) === -1) && (transaction.currency)){
-                totals.currencyList.push(currency_string);
-                totals.currencyTotals[currency_string]=0;
+            if ((jQuery.inArray(transaction.currency, currencies) === -1) && (transaction.currency)){
+                currencies.push(currency_string);
+                totals.debits[currency_string]=0;
+                totals.credits[currency_string]=0;
+                totals.wallet[currency_string]=0;
                 debits.currencySum[currency_string]=0;
                 credits.currencySum[currency_string]=0;
             }
@@ -41,66 +44,72 @@
             /* sum currencies individually */
             if (transaction.currency){
                 credits.currencySum[currency_string] += transaction.price;
-                totals.currencyTotals[currency_string] -= transaction.price; /* substract credits from total */
+                totals.credits[currency_string] += transaction.price;
+                if (value==='store') { /* Save added fundy by store for total spent calculation */
+                  totals.wallet[currency_string] += transaction.price;
+                }
             }
             credits.transactions.push(transaction);
             totals.creditsCount +=1;
-            totals.transactionCount += 1;
-            /*transaction.price *=-1;
-            totals.transactions.push(transaction);*/
           }
           /* Or debit */
           else if (jQuery(this).find('.transactionRowEvent').hasClass('charge')){
             /* sum currencies individually */
             if (transaction.currency){
                 debits.currencySum[currency_string] += transaction.price;
-                totals.currencyTotals[currency_string] += transaction.price;
+                totals.debits[currency_string] += transaction.price;
             }
             debits.transactions.push(transaction);
-            totals.transactionCount += 1;
-            /*totals.transactions.push(transaction);*/
+            totals.debitsCount += 1;
           }
           /* count CD keys*/
           else if (jQuery(this).find('.transactionRowEvent').hasClass('cdkey')){
             totals.cdkeyCount += 1;
-            totals.transactionCount += 1;
-            /*totals.transactions.push(transaction);*/
           }
       });
 
       /* Accounts output */
       total = '0.00';
-      if (totals.currencyList.length > 0) {
-        total = totals.currencyList[0] + formatNum(Math.round((debits.currencySum[totals.currencyList[0]]-credits.currencySum[totals.currencyList[0]])*100)/100);
+      if (currencies.length > 0) {
+        total = currencies[0] + formatNum(debits.currencySum[currencies[0]]-credits.currencySum[currencies[0]]);
       }
       jQuery('.sr'+value).remove();
       jQuery('.accountInfoBlock .accountBalance:last').before('<div class="accountRow accountBalance sr'+value+'"><div class="accountData price">'+total+'</div><div class="accountLabel">'+value.charAt(0).toUpperCase()+value.slice(1)+' Transactions <a id="srj'+value+'" href="javascript:(function(){if(jQuery(\'#srj'+value+'\').text()===\'[+]\') {jQuery(\'#srj'+value+'\').text(\'[-]\');jQuery(\'.srh'+value+'\').show();} else {jQuery(\'#srj'+value+'\').text(\'[+]\');jQuery(\'.srh'+value+'\').hide();}})();">[+]</a></div></div>');
-      jQuery('.accountInfoBlock .accountBalance:last').before('<table id="steam_'+value+'_account_table" class="sr'+value+' srh'+value+'" style="margin:7px 0;width:100%;border-collapse:collapse;"><tbody><tr style="border-top:2px solid #3c3d3e;border-bottom:1px solid #3c3d3e;}"><th style="border-right:2px solid #3c3d3e;width:50%">Debit</th><th style="width:50%">Credits</th></tr></tbody></table>');
-      jQuery(totals.currencyList).each(function(){
-        jQuery('#steam_'+value+'_account_table tbody').append('<tr><td style="text-align: left;border-right: 2px solid #3c3d3e;padding-left:.5em;">'+this+formatNum(Math.round(debits.currencySum[this]*100)/100)+'</td><td style="padding-left:.5em;">'+this+formatNum((Math.round(credits.currencySum[this]*100)/100)+'</td></tr>'));
+      jQuery('.accountInfoBlock .accountBalance:last').before('<table id="steam_'+value+'_account_table" class="sr'+value+' srh'+value+'" style="margin:7px 0;width:100%;border-collapse:collapse;"><tbody><tr style="border-top:2px solid #3c3d3e;border-bottom:1px solid #3c3d3e;}"><th style="border-right:2px solid #3c3d3e;width:50%">Debit</th><th>Credits</th></tr></tbody></table>');
+      jQuery(currencies).each(function(){
+        jQuery('#steam_'+value+'_account_table tbody').append('<tr><td style="border-right:2px solid #3c3d3e;padding-left:.5em;">'+this+formatNum(debits.currencySum[this])+'</td><td style="padding-left:.5em;">'+this+formatNum(credits.currencySum[this])+'</td></tr>');
       });
       jQuery('.accountInfoBlock .accountBalance:last').before('<div class="accountRow accountBalance sr'+value+' srh'+value+'" style="margin:0"><div class="accountData">'+debits.transactions.length+'</div><div class="accountLabel">'+((value==='store') ? 'Purchase' : 'Steam Wallet Debiting')+' Transactions</div></div>');
       jQuery('.accountInfoBlock .accountBalance:last').before('<div class="accountRow accountBalance sr'+value+' srh'+value+'" style="margin:0"><div class="accountData">'+credits.transactions.length+'</div><div class="accountLabel">Steam Wallet Crediting Transactions</div></div>');
-      jQuery('.accountInfoBlock .accountBalance:last').before('<div class="accountRow accountBalance sr'+value+' srh'+value+'" style="border-top:1px solid #3c3d3e;margin-bottom:16px"><div class="accountData">'+(debits.transactions.length+credits.transactions.length)+'</div><div class="accountLabel">Total Transactions</div></div>');
+      if (value==='store') jQuery('.accountInfoBlock .accountBalance:last').before('<div class="accountRow accountBalance sr'+value+' srh'+value+'" style="margin:0"><div class="accountData">'+totals.cdkeyCount+'</div><div class="accountLabel">CD Key Redemption Transactions</div></div>');
+      jQuery('.accountInfoBlock .accountBalance:last').before('<div class="accountRow accountBalance sr'+value+' srh'+value+'" style="border-top:1px solid #3c3d3e;margin-bottom:16px"><div class="accountData">'+(((value==='store') ? totals.cdkeyCount : 0)+debits.transactions.length+credits.transactions.length)+'</div><div class="accountLabel">Total Transactions</div></div>');
       jQuery('.srh'+value).hide();
     });
 
     /* Totals output */
     jQuery('.srtotal').remove();
     total = '0.00';
-    if (totals.currencyList.length > 0) {
-      total = formatNum(totals.currencyList[0] + formatNum(Math.round(totals.currencyTotals[totals.currencyList[0]]*100)/100));
+    if (currencies.length > 0) {
+      total = currencies[0] + formatNum(totals.debits[currencies[0]] - totals.credits[currencies[0]]);
     }
-    jQuery('.accountInfoBlock .accountBalance:last').before('<div class="accountRow accountBalance srtotal" style="border-top:1px solid #3c3d3e;padding-top:6px;"><div class="accountData price">'+total+'</div><div class="accountLabel">Total Spent <a id="srjtotal" href="javascript:(function(){if(jQuery(\'#srjtotal\').text()===\'[+]\') {jQuery(\'#srjtotal\').text(\'[-]\');jQuery(\'.srhtotal\').show();} else {jQuery(\'#srjtotal\').text(\'[+]\');jQuery(\'.srhtotal\').hide();}})();">[+]</a></div></div>');
-    jQuery('.accountInfoBlock .accountBalance:last').before('<table id="steam_total_account_table" class="srtotal srhtotal" style="margin:7px 0;width:100%;border-collapse:collapse;"><tbody><tr style="border-top:2px solid #3c3d3e;border-bottom:1px solid #3c3d3e;}"><th style="border-right:1px solid #3c3d3e;width:50%;text-align:right;padding-right:.5em">Currency</th><th style="text-align:left;padding-left:0.5em;">Total</th></tr></tbody></table>');
-    jQuery(totals.currencyList).each(function(){
-        jQuery('#steam_total_account_table tbody').append('<tr><td style="border-right:1px solid #3c3d3e;text-align:right;padding-right:.5em;">'+this+'</td><td style="padding-left:.5em;">'+formatNum(Math.round(totals.currencyTotals[this]*100)/100)+'</td></tr>');
+    jQuery('.accountInfoBlock .accountBalance:last').before('<div class="accountRow accountBalance srtotal" style="border-top:1px solid #3c3d3e;padding-top:6px;"><div class="accountData price">'+total+'</div><div class="accountLabel">Total <a id="srjtotal" href="javascript:(function(){if(jQuery(\'#srjtotal\').text()===\'[+]\') {jQuery(\'#srjtotal\').text(\'[-]\');jQuery(\'.srhtotal\').show();} else {jQuery(\'#srjtotal\').text(\'[+]\');jQuery(\'.srhtotal\').hide();}})();">[+]</a></div></div>');
+    jQuery('.accountInfoBlock .accountBalance:last').before('<table id="steam_total_account_table" class="srtotal srhtotal" style="margin:7px 0;width:100%;border-collapse:collapse;"><tbody><tr style="border-top:2px solid #3c3d3e;border-bottom:1px solid #3c3d3e;}"><th style="border-right:2px solid #3c3d3e;width:50%">Debit</th><th>Credit</th></tr></tbody></table>');
+    jQuery(currencies).each(function(){
+        jQuery('#steam_total_account_table tbody').append('<tr><td style="border-right:2px solid #3c3d3e;padding-left:.5em;">'+this+formatNum(totals.debits[this])+'</td><td style="padding-left:.5em;">'+this+formatNum(totals.credits[this])+'</td></tr>');
     });
-    jQuery('.accountInfoBlock .accountBalance:last').before('<div class="accountRow accountBalance srtotal srhtotal" style="margin:0"><div class="accountData">'+(totals.transactionCount-totals.cdkeyCount-totals.creditsCount)+'</div><div class="accountLabel">(Store/Market/Ingame) Buying Transactions</div></div>');
-    jQuery('.accountInfoBlock .accountBalance:last').before('<div class="accountRow accountBalance srtotal srhtotal" style="margin:0"><div class="accountData">'+totals.cdkeyCount+'</div><div class="accountLabel">CD Key Redemption Transactions</div></div>');
+    jQuery('.accountInfoBlock .accountBalance:last').before('<div class="accountRow accountBalance srtotal srhtotal" style="margin:0"><div class="accountData">'+totals.debitsCount+'</div><div class="accountLabel">(Store/Market/Ingame) Buying Transactions</div></div>');
     jQuery('.accountInfoBlock .accountBalance:last').before('<div class="accountRow accountBalance srtotal srhtotal" style="margin:0"><div class="accountData">'+totals.creditsCount+'</div><div class="accountLabel">Steam Wallet Crediting Transactions</div></div>');
-    jQuery('.accountInfoBlock .accountBalance:last').before('<div class="accountRow accountBalance srtotal srhtotal" style="border-top:1px solid #3c3d3e;"><div class="accountData">'+totals.transactionCount+'</div><div class="accountLabel">Total Transactions</div></div><div class="inner_rule srtotal"></div>');
+    jQuery('.accountInfoBlock .accountBalance:last').before('<div class="accountRow accountBalance srtotal srhtotal" style="margin:0"><div class="accountData">'+totals.cdkeyCount+'</div><div class="accountLabel">CD Key Redemption Transactions</div></div>');
+    jQuery('.accountInfoBlock .accountBalance:last').before('<div class="accountRow accountBalance srtotal srhtotal" style="border-top:1px solid #3c3d3e;"><div class="accountData">'+(totals.cdkeyCount+totals.debitsCount+totals.creditsCount)+'</div><div class="accountLabel">Total Transactions</div></div>');
     jQuery('.srhtotal').hide();
+
+    /* Total Invsested output */
+    total = '0.00';
+    if (currencies.length > 0) {
+      currentWallet = jQuery('.accountRow.accountBalance .accountData.price').filter(function() {return(jQuery(this).parent().attr('class').replace(/accountRow|accountBalance|\s*/g, "") == '');}).text(); /* Filter the only element that has only the original CSS classes */
+      total = currencies[0] + formatNum(totals.debits[currencies[0]] - totals.credits[currencies[0]] + totals.wallet[currencies[0]] + toNumber(currentWallet)) + ((currencies.length > 1) ? 'Please contact the developer. You have more than one currency in you account, but only the first one found is displayed.' : '');
+    }
+    jQuery('.accountInfoBlock .accountBalance:last').before('<div class="inner_rule srtotal"></div><div class="accountRow accountBalance srtotal"><div class="accountData price">'+total+'</div><div class="accountLabel">Total Invested (real money)</div></div><div class="inner_rule srtotal"></div>');
 }
   else{
     alert('This script only works at:\n\nhttps://store.steampowered.com/account/');
@@ -108,6 +117,7 @@
 /*});*/
 
 function formatNum(num) {
+  num = Math.round(num*100)/100;
   sp = num.toString().split('.');
   switch (sp.length) {
     case 0:
@@ -124,4 +134,8 @@ function formatNum(num) {
       return num;
       break;
   }
+}
+
+function toNumber(price) {
+  return Number(price.replace(/[^\d.,-]/g, '').replace('\,','\.').replace(/-/g,'0'));
 }
